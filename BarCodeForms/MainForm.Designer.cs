b4f81211f9e6ff7139aded4e9e3b65f47bc81c92
Kubariev Ilya <polcrazpolcraz@gmail.com>
2021-01-25 20:05:39 +0200

    Add project files.

---
+++ BarCodeForms/MainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace BarCodeForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMakeBarcode = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMakeBarcode
+            // 
+            this.btnMakeBarcode.Location = new System.Drawing.Point(12, 100);
+            this.btnMakeBarcode.Name = "btnMakeBarcode";
+            this.btnMakeBarcode.Size = new System.Drawing.Size(121, 42);
+            this.btnMakeBarcode.TabIndex = 0;
+            this.btnMakeBarcode.Text = "Старт";
+            this.btnMakeBarcode.UseVisualStyleBackColor = true;
+            this.btnMakeBarcode.Click += new System.EventHandler(this.btnMakeBarcode_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Выберите тип";
+            // 
+            // cmbType
+            // 
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Location = new System.Drawing.Point(12, 71);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(121, 23);
+            this.cmbType.TabIndex = 3;
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(12, 27);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(121, 23);
+            this.txtText.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(139, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 130);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 154);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.cmbType);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMakeBarcode);
+            this.Name = "MainForm";
+            this.Text = "BarCode";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMakeBarcode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ BarCodeForms/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using BarcodeLib;
+
+namespace BarCodeForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMakeBarcode_Click(object sender, EventArgs e)
+        {
+            var text = txtText.Text;
+            var barcode = new Barcode();
+            var barcodeImage = barcode.Encode(TYPE.CODE128, text, Color.Black, Color.White, pictureBox1.Width - 20, pictureBox1.Height - 20);
+            pictureBox1.Image = barcodeImage;
+        }
+    }
+}
